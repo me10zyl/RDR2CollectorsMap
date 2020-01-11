302524b5738c9ff887d222482bee7aeeb8e8b23e
zyl <me10zyl@qq.com>
2020-01-11 22:01:23 +0800

    update

--- assets/js/zyl.js
+++ assets/js/zyl.js
@@ -10,6 +10,7 @@ var zyl  = {
     init : function(){
         this.bindEvents();
         this.loadData();
+        this.calcSearch();
     },
     loadData:function(){
         $(".input-search").val(localStorage['zyl-input']);
@@ -20,10 +21,24 @@ var zyl  = {
         $(".input-search,#hideGottenNumber").keyup(function(){
             zyl.save();
         });
+        $(".addToMap").each(function(){
+            this.addEventListener("click", function(e){
+            var category = $(this).closest('.menu-option').data('type');
+            if(!$(this).hasClass('enable')){
+                $(this).addClass('enable')
+                zyl.addToSearch(category);
+            }else{
+                $(this).removeClass('enable');
+                zyl.removeToSearch(category);
+            }
+
+            e.stopPropagation()
+        })});
     },
     save : function(){
         localStorage['zyl-input'] =  $(".input-search").val();
         localStorage['hideGottenNumber'] = $("#hideGottenNumber").val();
+        this.calcSearch();
     },
     calcCollected:function(){
         console.log('calcCollected')
@@ -31,8 +46,12 @@ var zyl  = {
         $(".menu-option").each(function(e){
             var type = $(this).data('type');
             if(allCategories.indexOf(type) > -1){
-                $(this).find("span+.calcAmount").remove();
-                $(this).find("span").after("<div style='margin-left: 10px' class='calcAmount'>" + zyl.getCollectedAmountOfCategory(type) + "/" + zyl.getTotalSizeOfCategory(type) +  "</div>");
+                var exist = $(this).find("span+.calcAmount");
+                var string = zyl.getCollectedAmountOfCategory(type) + "/" + zyl.getTotalSizeOfCategory(type);
+                if(exist){
+                    exist.find('.nums').text(string)
+                }
+                $(this).find("span").after("<div style='margin-left: 10px' class='calcAmount'><span class='nums'>" + string +  "</span><span  class='addToMap'>+</span></div>");
             }
         })
     },
@@ -57,13 +76,25 @@ var zyl  = {
         return amount;
     },
 
-    hideGotten : function(){
+    removeToSearch : function(category){
+        var split = $(".input-search").val().split(";");
+        if(split.length > 0) {
+            var titles = this.getMissingItems(category);
+            split = split.filter(e=>titles.indexOf(e) < 0);
+            console.log(split, titles)
+            $(".input-search").val(split.join(";"))
+            $(document.querySelector('.input-search')).trigger('input');
+        }
+        zyl.save();
+    },
+
+    getMissingItems : function(category){
         var markerCollected = {
 
         }
-        var size = $("#hideGottenNumber").val() || 3;
+        var categories = [category];
         $.each( MapBase.markers, function(key, marker){
-            if(marker.amount == 0 && zyl.getAllCategories().indexOf(marker.category) > -1){
+            if(marker.amount == 0 && categories.indexOf(marker.category) > -1){
                 var collectible = marker.subdata || marker.text;
                 if(collectible.match('_\d')){
                     return true;
@@ -75,9 +106,6 @@ var zyl  = {
         });
         var titles = [];
         $.each(markerCollected, function(k, v){
-            if(zyl.getTotalSizeOfCategory(v.category) - zyl.getCollectedAmountOfCategory(v.category) > size){
-                return true;
-            }
             var title = v.title;
             if(title.indexOf('#') > -1){
                 title = title.substring(0, title.indexOf('#')).trim();
@@ -86,6 +114,60 @@ var zyl  = {
                 titles.push(title);
             }
         });
+        return titles;
+    },
+
+    calcSearch : function () {
+        var val = $(".input-search").val();
+        var split = val.split(';');
+        var allCategories = this.getAllCategories();
+        for(var i in allCategories){
+            var category = allCategories[i];
+            var missingItems = this.getMissingItems(category);
+            var allEq = true;
+            missingItems.forEach(function(e){
+                if(split.indexOf(e) < 0){
+                    allEq = false;
+                }
+            });
+            var find = $(".menu-option[data-type=" + category+"]").find(".addToMap");
+            if(allEq){
+                find.addClass('enable');
+            }else{
+                find.removeClass('enable');
+            }
+        }
+    },
+
+    addToSearch : function (category) {
+        var titles = this.getMissingItems(category);
+        var val = $(".input-search").val();
+        var values = val.split(";");
+        if(values.length > 0) {
+            titles = titles.filter(function (t) {
+                return values.indexOf(t) < 0;
+            });
+            if(titles.length > 0) {
+                $(".input-search").val(val + ";" + titles.join(";"))
+            }
+        }else {
+            $(".input-search").val(titles.join(";"))
+        }
+        $(document.querySelector('.input-search')).trigger('input');
+        zyl.save()
+    },
+
+    hideGotten : function(){
+        var size = $("#hideGottenNumber").val() || 3;
+        var allCategories = this.getAllCategories();
+        var titles = [];
+        for(var i in allCategories){
+            var category = allCategories[i];
+            var missingItems = this.getMissingItems(category);
+            if((zyl.getTotalSizeOfCategory(category) - zyl.getCollectedAmountOfCategory(category)) <= size){
+                titles = titles.concat(missingItems);
+            }
+        }
         $(".input-search").val(titles.join(";"))
         $(document.querySelector('.input-search')).trigger('input');
         zyl.save()
